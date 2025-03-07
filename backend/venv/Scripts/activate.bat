a1a91f1b6a8859d88d540fa7d206c006ea569ea2
John Michael Lim <johnmichaellim041202@gmail.com>
2025-03-07 14:32:59 +0800

    reboot venv

--- backend/venv/Scripts/activate.bat
+++ backend/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\mrhum\Documents\2fd\louykayko\SwiftShield\backend\venv
+set VIRTUAL_ENV=C:\Users\mrhum\Documents\2fd\kapoya\SwiftShield\backend\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
